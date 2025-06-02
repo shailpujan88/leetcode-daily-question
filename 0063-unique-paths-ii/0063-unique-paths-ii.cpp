9ca081fdb31261326954f292f6cdcbfd0faadb67
Shail Pujan <114280481+shailpujan88@users.noreply.github.com>
2025-06-02 22:53:54 +0530

    Time: 0 ms (100%), Space: 12 MB (28.23%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m=obstacleGrid.size();
-        int n=obstacleGrid[0].size();
-           vector<vector<int>> dp(m,(vector<int>(n)));
-           if(m==1 && n==1) return 0;
-           if(m==0 && n==0) return 1;
-            dp[0][0] = 1;
-            for(int i=0;i<m;i++){
-                for(int j =0;j<n;j++){
-                    int up=0;
-                    int left=0;
-                    if(i-1>=0 && obstacleGrid[i][j]==0)  up = dp[i-1][j];
-                    if(j-1>=0 && obstacleGrid[i][j]==0)  left = dp[i][j-1];
-                    int sum=0;
-                    sum+= up+left;
-                    dp[i][j]+=sum;
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        // Fix: return 0 if start is an obstacle
+        if (obstacleGrid[0][0] == 1) return 0;
+        dp[0][0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // Fix: skip obstacle cells
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+
+                int up = 0, left = 0;
+                if (i - 1 >= 0 && obstacleGrid[i][j]==0) up = dp[i - 1][j];
+                if (j - 1 >= 0 && obstacleGrid[i][j]==0) left = dp[i][j - 1];
+                int sum=0;
+                sum += up + left;
+                dp[i][j]+=sum;
                 }
             }
-            return dp[m-1][n-1];
+
+        return dp[m - 1][n - 1];
+    }
+};
 
 //         vector<int> prev(n, 1); // Base case: first row is all 1s
 
@@ -31,5 +41,4 @@ public:
 //         return prev[n - 1];
 //     }
 // };
-    }
-};
+    
