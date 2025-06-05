395ff14a745e6a163c0dfced3e6afa36c1501e47
Shail Pujan <114280481+shailpujan88@users.noreply.github.com>
2025-06-05 15:15:58 +0530

    Time: 1 ms (23.37%), Space: 12.6 MB (85.71%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -15,18 +15,41 @@ public:
         // memo
         ///---->tc-o(n)(n)
         //--->sc o(n*n) o(n)
-        int m = triangle.size();
-        vector<vector<int>> dp(m, vector<int>(m, 0));
+//-------------------------------------------------------------------------------->
+        // int m = triangle.size();
+        // vector<vector<int>> dp(m, vector<int>(m, 0));
+        // for (int j = 0; j < m; j++) {
+        //     dp[m-1][j]=triangle[m-1][j];
+        // }
+        // for (int i = m - 2; i >= 0; i--) {
+        //     for (int j = i; j >= 0; j--) {
+        //         int down = triangle[i][j] + dp[i + 1][j];
+        //         int diagonal = triangle[i][j] + dp[i + 1][j + 1];
+        //         dp[i][j]=min(down,diagonal);
+        //     }
+        // }
+        // return dp[0][0];
+    // tabulation
+    //tc-o(n*n)
+    //sc-o(n*n)
+//------------------------------------------------------------------------------------->
+int m = triangle.size();
+        vector<int> front(m, 0);
+         vector<int> curr(m, 0);
         for (int j = 0; j < m; j++) {
-            dp[m-1][j]=triangle[m-1][j];
+           front[j]=triangle[m-1][j];
         }
         for (int i = m - 2; i >= 0; i--) {
             for (int j = i; j >= 0; j--) {
-                int down = triangle[i][j] + dp[i + 1][j];
-                int diagonal = triangle[i][j] + dp[i + 1][j + 1];
-                dp[i][j]=min(down,diagonal);
+                int down = triangle[i][j] + front[j];
+                int diagonal = triangle[i][j] + front[j + 1];
+                curr[j]=min(down,diagonal);
             }
+            front=curr;
         }
-        return dp[0][0];
+        return front[0];
+        ////space optimisation
+        //tc-o(n*n)
+        //sc(o(n)
     }
 };
