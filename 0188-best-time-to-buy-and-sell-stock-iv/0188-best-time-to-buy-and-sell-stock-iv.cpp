4e9f9dadc38c5905abfcf4caa56fe5ac34a77edd
Shail Pujan <114280481+shailpujan88@users.noreply.github.com>
2025-06-21 18:13:17 +0530

    Time: 5 ms (66.79%), Space: 18.4 MB (24.17%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -3,29 +3,26 @@ public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
         if (n == 0 || k == 0) return 0;
-
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(k + 1, 0)));
-
+        vector<vector<vector<int>>> dp(
+            n + 1, vector<vector<int>>(2, vector<int>(k+1, 0)));
         for (int ind = n - 1; ind >= 0; ind--) {
             for (int buy = 0; buy <= 1; buy++) {
                 for (int cap = 1; cap <= k; cap++) {
                     if (buy == 1) {
-                        dp[ind][buy][cap] = max(-prices[ind] + dp[ind + 1][0][cap],
-                                                 dp[ind + 1][1][cap]);
+                        dp[ind][buy][cap] =
+                            max(-prices[ind] + dp[ind + 1][0][cap],
+                                0 + dp[ind + 1][1][cap]);
                     } else {
-                        dp[ind][buy][cap] = max(prices[ind] + dp[ind + 1][1][cap - 1],
-                                                 dp[ind + 1][0][cap]);
+                        dp[ind][buy][cap] =
+                            max(prices[ind] + dp[ind + 1][1][cap - 1],
+                                dp[ind + 1][0][cap]);
                     }
                 }
             }
         }
-
-        return dp[0][1][k];  // Start with "can buy" state
-    }
-};
-
-    //------------------------------------------------------------------------>
-   // tabulation
+        return dp[0][1][k];
+        //------------------------------------------------------------------------>
+        // tabulation
         // int n=prices.size();
         //  vector<vector<int>>cur(2, vector<int>(3, 0));
         //   vector<vector<int>>after(2, vector<int>(3, 0));
@@ -43,3 +40,5 @@ public:
         //     after=cur;
         // }
         // return after[1][2];
+    }
+};
