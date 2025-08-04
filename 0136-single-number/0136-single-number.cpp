3bd1cf6658987d869946e79207988a6c507c3962
Shail Pujan <114280481+shailpujan88@users.noreply.github.com>
2025-08-04 22:32:10 +0530

    Time: 0 ms (100%), Space: 19.6 MB (99.93%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        // -->   take Xorr as where we store our vALUE OF xor we are doing
+        // -->   we will itterate from i=0->n
+        // -->   and we will do xorr^nums[i]
+        //  -->  and then return Xorr
+        //  -->  eg [4,1,2,1,2]
+        //  -->  4^1^2^1^2
+        //  -->  4^1^1^2^2 -->1^1=0 && 2^2=0
+        //  -->  4^0^0
+        //   --> 4-->here we get our single element in our arr
         int Xorr=0;
         for(int i=0;i<nums.size();i++){
             Xorr=Xorr^nums[i];
