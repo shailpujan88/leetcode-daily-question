2b4d06745a6fd41f0e14d32fdb718911a238bd1c
Shail Pujan <114280481+shailpujan88@users.noreply.github.com>
2025-08-12 00:36:25 +0530

    Time: 0 ms (100%), Space: 21.7 MB (92.35%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        // int n = nums.size();
+        // sort(nums.begin(),nums.end());
+        // int ans=0;
+        // for (int i = 0; i < n; i++) {
+        //     if (nums[i] != i + 1){
+        //         ans=i+1;
+        //     }
+        // }
+        // return ans;
+        //tc=nlogn
+        //sc=o(1)
+        //optimal
+        //by using xor
         int n = nums.size();
-        sort(nums.begin(),nums.end());
-        int ans=0;
+        int xorAll = 0;
         for (int i = 0; i < n; i++) {
-            if (nums[i] != i + 1){
-                ans=i+1;
-            }
+            xorAll ^= i ^ nums[i];
         }
-        return ans;
+        
+        xorAll ^= n;
+        
+        return xorAll;
     }
 };
+
+    
