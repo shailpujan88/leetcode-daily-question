9de745dbcec881aa6afcbed908c12649eeb0b186
Shail Pujan <114280481+shailpujan88@users.noreply.github.com>
2025-08-12 22:57:57 +0530

    Time: 0 ms (100%), Space: 11.5 MB (97.42%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,15 +1,34 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        //if the sort library is allowed
-       // sort(nums.begin(),nums.end());
-       //if no allowed the sort library then the 
-        int n = nums.size();
-        for(int i = 0; i < n - 1; i++) {
-            for(int j = i + 1; j < n; j++) {
-                if(nums[i] > nums[j]) {
-                    swap(nums[i], nums[j]);
-                }
+        // if the sort library is allowed
+        // sort(nums.begin(),nums.end());
+        // if no allowed the sort library then the below code is brute force
+        // code
+        // int n = nums.size();
+        // for(int i = 0; i < n - 1; i++) {
+        //     for(int j = i + 1; j < n; j++) {
+        //         if(nums[i] > nums[j]) {
+        //             swap(nums[i], nums[j]);
+        //         }
+        //     }
+        // }
+        // ✅ Time Complexity: O(n²)
+        //✅ Space Complexity: O(1)
+        // the below code is optimal approach the DUTCH NATION FLAG ALGORITHM
+        int low = 0, mid = 0,
+            high = nums.size() - 1; // ✅ Time Complexity: O(n)
+                                    // \U0001f9ee Space Complexity // No extra arrays are used.
+        while (mid <= high) {
+            if (nums[mid] == 2) {
+                swap(nums[mid], nums[high]);
+                high--;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else { // nums[mid]==0
+                swap(nums[low], nums[mid]);
+                mid++;
+                low++;
             }
         }
     }
