a2cbb2fd98e40cdb6205d3c3adcc7f1c2b66321f
Shail Pujan <114280481+shailpujan88@users.noreply.github.com>
2025-08-14 00:47:47 +0530

    Time: 0 ms (100%), Space: 71.6 MB (80.52%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int maxsum=nums[0];
-        int curr=0;
-        for(int i=0;i<nums.size();i++){
+int maxsum=nums[0];
+int curr=nums[0];//So the simple rule we can take curr=0 but if we take 0 we are doing cgeating beacuse we should have one element always in subarray
+//Start with the first real pile you see//not an imaginary pile of zero. \U0001f36c
+        for(int i=1;i<nums.size();i++){
             curr=max(nums[i],curr+nums[i]);
             maxsum=max(curr,maxsum);
         }
