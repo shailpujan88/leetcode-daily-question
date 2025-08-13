0b23f009ef55a97156339389aee78b5e6b7b33f2
Shail Pujan <114280481+shailpujan88@users.noreply.github.com>
2025-08-14 00:26:41 +0530

    Time: 0 ms (100%), Space: 28 MB (86.85%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-         int cnt =0;
-	int ele;
-	for(int i=0;i<nums.size();i++){
-		if(cnt==0){
-			cnt=1;
-			ele=nums[i];
-		
-		}
-		else{
-			if(nums[i]==ele){
-				cnt++;
-			}
-			else{
-				cnt--;
-			}
-		}
-		
-	}
-	int cnt1=0;
-	for(int i=0;i<nums.size();i++){
-		if(nums[i]==ele){
-				cnt1++;
-			}
-			if(cnt1>(nums.size()/2)){
-				return ele;
-			}
-	}
-	return -1;
+        // int n =nums.size();
+        // int ans=0;
+        // sort(nums.begin(),nums.end());
+        // ans=nums[n/2];
+        // return ans;
+        int candidate=nums[0];
+        int count=1;
+        for(int i=1;i<nums.size();i++){
+            if(count<=0){
+                candidate=nums[i];
+            }
+            if(candidate==nums[i]){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return candidate;
     }
 };
-
+//tc=o(n)
+//sc=o(1)
+// i	nums[i]	candidate	count	Action
+// 0	  2		   2         1	   Init candidate = 2, count = 1
+// 1	  2		   2         2     Same as candidate → count++
+// 2	  1		   2 	     1     Different → count--
+// 3	  1		   2	     0     Different → count--, now count=0
+// 4	  1		   1	     1     count <= 0, so candidate = 1, count++
+// 5	  2		   1	     0     Different → count--, count=0
+// 6	  2		   2	     1     count <= 0, candidate = 2, count++
