38cb381ad13adfaad49f461e53bd3d8aea3854d4
Shail Pujan <114280481+shailpujan88@users.noreply.github.com>
2025-08-23 16:37:19 +0530

    Time: 8 ms (62.5%), Space: 44.9 MB (43.51%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -14,5 +14,18 @@ public:
             r++;
         }
         return l;
+        // vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=m;j++){
+        //         if(g[i-1]<=s[j-1]){
+        //             dp[i][j]=max(dp[i-1][j-1]+1,dp[i-1][j]);
+        //         }
+        //         else{
+        //             dp[i][j]=dp[i-1][j];
+        //         }
+        //     }
+        // }
+        //    return dp[n][m];  ///time limit execced in dp
+
     }
 };
