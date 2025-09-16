8d68e1860a6134babde22f4fb9231811aaae2f1a
Shail Pujan <114280481+shailpujan88@users.noreply.github.com>
2025-09-16 19:22:23 +0530

    Time: 1 ms (40.44%), Space: 17 MB (55.17%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -5,7 +5,7 @@ public:
         int m = grid[0].size();
         queue<pair<pair<int, int>, int>> q;
         vector<vector<int>> vis(n, vector<int>(m, 0));
-        int cntfresh=0;//
+        int cntfresh=0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 2) {
@@ -19,7 +19,6 @@ public:
             }
         }
         int time = 0;
-        int cnt=0;//
         int delrow[] = {-1, 0, +1, 0};
         int delcol[]= {0, +1, 0, -1};
         while (!q.empty()) {
@@ -35,20 +34,17 @@ public:
                     grid[nrow][ncol] == 1 && vis[nrow][ncol]== 0) {
                     q.push({{nrow, ncol}, time + 1});
                     vis[nrow][ncol] = 2;
-                    cnt++;//
                 }
             }
         }
-        // for (int i = 0; i < n; i++) {
-        //     for (int j = 0; j < m; j++) {
-        //         if (vis[i][j] != 2 && grid[i][j] == 1) {
-        //             return -1;
-        //         }
-        //     }
-        // }//this is all a way too check any organe remoain unrotten
-        if(cnt!=cntfresh) return -1;//
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (vis[i][j] != 2 && grid[i][j] == 1) {
+                    return -1;
+                }
+            }
+        }
 
-        // where this is u can remove the // part and 
         return time;
     }
 };
